23f127690fb9f7449510e91472ef332710969b46
mg198@hdm-stuttgart.de <mg198@hdm-stuttgart.de>
2019-05-28 13:27:47 +0200

    Added SplashScreen

--- app/src/main/java/de/hdmstuttgart/parkspot/MainActivity.kt
+++ app/src/main/java/de/hdmstuttgart/parkspot/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
