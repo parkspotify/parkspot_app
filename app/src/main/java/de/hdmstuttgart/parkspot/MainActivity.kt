92d234e45fb664d007c587133accd7856043666d
mg198@hdm-stuttgart.de <mg198@hdm-stuttgart.de>
2019-05-28 12:26:41 +0200

    Removed unneeded boiler plate code

--- app/src/main/java/de/hdmstuttgart/parkspot/MainActivity.kt
+++ app/src/main/java/de/hdmstuttgart/parkspot/MainActivity.kt
@@ -1,11 +1,9 @@
 package de.hdmstuttgart.parkspot
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
