7e43a7a792ea822cc733c88ce9f9db7ded6f3cf4
Nils Mursinsky <nm052@hdm-stuttgart.de>
2019-06-02 22:30:01 +0200

    start login activity after successfull registration

--- app/src/main/java/de/hdmstuttgart/parkspot/usermanagement/UserRegister.java
+++ app/src/main/java/de/hdmstuttgart/parkspot/usermanagement/UserRegister.java
@@ -1,13 +1,16 @@
 package de.hdmstuttgart.parkspot.usermanagement;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
+import de.hdmstuttgart.parkspot.activities.LoginActivity;
+import de.hdmstuttgart.parkspot.activities.RegisterActivity;
 import de.hdmstuttgart.parkspot.networking.Client;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
 import java.io.IOException;
 
 public class UserRegister {
@@ -26,6 +29,9 @@ public class UserRegister {
                     try {
                         String resp = response.body().string();
                         Toast.makeText(context, resp, Toast.LENGTH_LONG).show();
+
+                        context.startActivity(new Intent(context, LoginActivity.class));
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
