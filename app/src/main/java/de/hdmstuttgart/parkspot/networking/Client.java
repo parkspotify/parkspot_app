69945a91c2b00b7d9513416eef3afa2a2424ee2f
Mursinsky Nils <nm051@hdm-stuttgart.de>
2019-08-26 17:58:46 +0200

    Base URL imported from Constants and software license added to each file

--- app/src/main/java/de/hdmstuttgart/parkspot/networking/Client.java
+++ app/src/main/java/de/hdmstuttgart/parkspot/networking/Client.java
@@ -1,11 +1,29 @@
 package de.hdmstuttgart.parkspot.networking;
 
+import de.hdmstuttgart.parkspot.Constants;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * This file is part of Parkspot.      
+ *
+ * Parkspot is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation version 3 of the License.
+ * Parkspot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Parkspot. 
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * Copyright (c) 2019, Hochschule der Medien
+ * Author: Nils Mursinsky
+ */
+
 public class Client {
 
-    private static final String BASE_URL = "http://192.168.1.101:8000/";
+    private final String BASE_URL = Constants.BASE_URL;
 
     private static Client mRetrofitInstance;
     private Retrofit retrofit;
