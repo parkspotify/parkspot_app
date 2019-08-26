f189863b2b28a5ec2eeb6fbcb0f5fbc506156b35
Mursinsky Nils <nm051@hdm-stuttgart.de>
2019-08-26 18:00:23 +0200

    Licensing added to new files

--- app/src/main/java/de/hdmstuttgart/parkspot/usermanagement/UserLogin.java
+++ app/src/main/java/de/hdmstuttgart/parkspot/usermanagement/UserLogin.java
@@ -15,6 +15,23 @@ import retrofit2.Response;
 
 import java.io.IOException;
 
+/**
+ * This file is part of Parkspot.      
+ *
+ * Parkspot is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation version 3 of the License.
+ * Parkspot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Parkspot. 
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * Copyright (c) 2019, Hochschule der Medien
+ * Author: Nils Mursinsky
+ */
+
 public class UserLogin {
 
     User mUser = new User();
--- app/src/main/java/de/hdmstuttgart/parkspot/usermanagement/UserRegister.java
+++ app/src/main/java/de/hdmstuttgart/parkspot/usermanagement/UserRegister.java
@@ -12,6 +12,24 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import java.io.IOException;
 
+/**
+ * This file is part of Parkspot.      
+ *
+ * Parkspot is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation version 3 of the License.
+ * Parkspot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Parkspot. 
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * Copyright (c) 2019, Hochschule der Medien
+ * Author: Nils Mursinsky
+ */
+
+
 public class UserRegister {
 
     public void registerNewUser(String mail, String password, final Context context) {
@@ -19,7 +37,7 @@ public class UserRegister {
         Call<ResponseBody> call = Client
                 .getInstance()
                 .getApi()
-                .userregister(mail, password);
+                .register(mail, password);
 
         call.enqueue(new Callback<ResponseBody>() {
             @Override
